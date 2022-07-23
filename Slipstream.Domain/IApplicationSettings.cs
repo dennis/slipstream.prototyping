568eb1130525e41cba335108ba760a8e447c81ad
Dennis Møllegaard Pedersen <dennis@moellegaard.dk>
2022-07-24 01:48:26 +0200

    Simplify interface

--- Slipstream.Domain/IApplicationSettings.cs
+++ Slipstream.Domain/IApplicationSettings.cs
@@ -1,28 +1,7 @@
-﻿using Slipstream.Domain.Actions;
-using Slipstream.Domain.Instances;
-using Slipstream.Domain.Rules;
-using Slipstream.Domain.Triggers;
-using Slipstream.Domain.ValueObjects;
-
-namespace Slipstream.Domain;
+﻿namespace Slipstream.Domain;
 
 public interface IApplicationSettings
 {
-    IEnumerable<(EntityTypeName, EntityName)> ReadInstances();
-    void SaveInstance(IInstance instance);
-    string LoadInstance(EntityTypeName pluginName, EntityName instanceName);
-
-    IEnumerable<(EntityTypeName, EntityName)> ReadTriggers();
-    void SaveTrigger(ITrigger trigger);
-    string LoadTrigger(EntityTypeName triggerTypeName, EntityName triggerName);
-
-    IEnumerable<(EntityTypeName, EntityName)> ReadRules();
-    void SaveRule(IRule rule);
-    public string LoadRule(EntityName entityName);
-
-    IEnumerable<(EntityTypeName, EntityName)> ReadActions();
-    void SaveAction(IAction action);
-    public string LoadAction(EntityTypeName triggerTypeName, EntityName entityName);
     void Load(Action<string> print, Action<string> error);
     void Save(Action<string> print);
 }
--- Slipstream.Infrastructure/ApplicationSettings.cs
+++ Slipstream.Infrastructure/ApplicationSettings.cs
@@ -5,8 +5,6 @@ using Slipstream.Domain.Rules;
 using Slipstream.Domain.Triggers;
 using Slipstream.Domain.ValueObjects;
 
-using System.Data;
-
 namespace Slipstream.Infrastructure;
 
 public class ApplicationSettings : IApplicationSettings
@@ -23,81 +21,6 @@ public class ApplicationSettings : IApplicationSettings
 
         _registry = registry;
         _ruleFactory = ruleFactory;
-    }
-
-    public void SaveInstance(IInstance instance)
-    {
-        var json = _registry.InstanceContainer[instance.TypeName].ConfigurationJsonEncoder(instance.Configuration);
-        SaveEntity("save/instances", (string)instance.TypeName, instance.Name, json);
-    }
-        
-    public IEnumerable<(EntityTypeName, EntityName)> ReadInstances()
-        => ReadEntities("save/instances");
-
-    public string LoadInstance(EntityTypeName entityTypeName, EntityName entityName)
-        => LoadEntity("save/instances/", entityTypeName, entityName);
-
-    public void SaveTrigger(ITrigger trigger)
-    {
-        var json = _registry.TriggerContainer.Types[trigger.TypeName].ConfigurationJsonEncoder(trigger.Configuration);
-        SaveEntity("save/triggers", (string)trigger.TypeName, trigger.Name, json);
-    }
-
-    public IEnumerable<(EntityTypeName, EntityName)> ReadTriggers()
-        => ReadEntities("save/triggers");
-
-    public string LoadTrigger(EntityTypeName entityTypeName, EntityName entityName)
-        => LoadEntity("save/triggers/", entityTypeName, entityName);
-
-    public void SaveRule(IRule rule)
-    {
-        var json = _ruleFactory.ConfigurationJsonEncoder(rule.Configuration);
-        SaveEntity("save/rules", (string)rule.TypeName, rule.Name, json);
-    }
-
-    public IEnumerable<(EntityTypeName, EntityName)> ReadRules()
-        => ReadEntities("save/rules");
-
-    public string LoadRule(EntityName entityName)
-        => LoadEntity("save/rules", "rule", entityName);
-
-
-    public IEnumerable<(EntityTypeName, EntityName)> ReadActions()
-        => ReadEntities("save/actions");
-
-    public void SaveAction(IAction action)
-    {
-        var json = _registry.ActionContainer.Types[action.TypeName].ConfigurationJsonEncoder(action.Configuration); 
-        SaveEntity("save/actions", (string)action.TypeName, action.Name, json);
-    }
-
-    public string LoadAction(EntityTypeName entityTypeName, EntityName entityName)
-        => LoadEntity("save/actions", entityTypeName, entityName);
-
-    private static string LoadEntity(string rootDirectory, EntityTypeName entityTypeName, EntityName entityName)
-        => File.ReadAllText($"{rootDirectory}/{entityTypeName}/{entityName}.json");
-
-    private static IEnumerable<(EntityTypeName, EntityName)> ReadEntities(string rootDirectory)
-    {
-        var entries = new List<(EntityTypeName, EntityName)>();
-
-        foreach (var entityTypeDirectory in Directory.GetDirectories(rootDirectory))
-        {
-            var entityType = Path.GetFileName(entityTypeDirectory);
-            foreach (var entityFilename in Directory.GetFiles(entityTypeDirectory))
-            {
-                var entityName = Path.GetFileNameWithoutExtension(Path.GetFileName(entityFilename));
-                entries.Add((entityType, entityName));
-            }
-        }
-
-        return entries;
-    }
-
-    private static void SaveEntity(string rootDirectory, EntityTypeName entityTypeName, EntityName entityName, string content)
-    {
-        Directory.CreateDirectory($"{rootDirectory}/{entityTypeName}");
-        File.WriteAllText($"{rootDirectory}/{entityTypeName}/{entityName}.json", content);
     }
 
     public void Load(Action<string> print, Action<string> error)
@@ -202,4 +125,79 @@ public class ApplicationSettings : IApplicationSettings
 
         print("Done");
     }
+
+    private void SaveInstance(IInstance instance)
+    {
+        var json = _registry.InstanceContainer[instance.TypeName].ConfigurationJsonEncoder(instance.Configuration);
+        SaveEntity("save/instances", (string)instance.TypeName, instance.Name, json);
+    }
+
+    private static IEnumerable<(EntityTypeName, EntityName)> ReadInstances()
+        => ReadEntities("save/instances");
+
+    private static string LoadInstance(EntityTypeName entityTypeName, EntityName entityName)
+        => LoadEntity("save/instances/", entityTypeName, entityName);
+
+    private void SaveTrigger(ITrigger trigger)
+    {
+        var json = _registry.TriggerContainer.Types[trigger.TypeName].ConfigurationJsonEncoder(trigger.Configuration);
+        SaveEntity("save/triggers", (string)trigger.TypeName, trigger.Name, json);
+    }
+
+    private static IEnumerable<(EntityTypeName, EntityName)> ReadTriggers()
+        => ReadEntities("save/triggers");
+
+    private static string LoadTrigger(EntityTypeName entityTypeName, EntityName entityName)
+        => LoadEntity("save/triggers/", entityTypeName, entityName);
+
+    private void SaveRule(IRule rule)
+    {
+        var json = _ruleFactory.ConfigurationJsonEncoder(rule.Configuration);
+        SaveEntity("save/rules", (string)rule.TypeName, rule.Name, json);
+    }
+
+    private static IEnumerable<(EntityTypeName, EntityName)> ReadRules()
+        => ReadEntities("save/rules");
+
+    private static string LoadRule(EntityName entityName)
+        => LoadEntity("save/rules", "rule", entityName);
+
+    private static IEnumerable<(EntityTypeName, EntityName)> ReadActions()
+        => ReadEntities("save/actions");
+
+    private void SaveAction(IAction action)
+    {
+        var json = _registry.ActionContainer.Types[action.TypeName].ConfigurationJsonEncoder(action.Configuration); 
+        SaveEntity("save/actions", (string)action.TypeName, action.Name, json);
+    }
+
+    private static string LoadAction(EntityTypeName entityTypeName, EntityName entityName)
+        => LoadEntity("save/actions", entityTypeName, entityName);
+
+    private static string LoadEntity(string rootDirectory, EntityTypeName entityTypeName, EntityName entityName)
+        => File.ReadAllText($"{rootDirectory}/{entityTypeName}/{entityName}.json");
+
+    private static IEnumerable<(EntityTypeName, EntityName)> ReadEntities(string rootDirectory)
+    {
+        var entries = new List<(EntityTypeName, EntityName)>();
+
+        foreach (var entityTypeDirectory in Directory.GetDirectories(rootDirectory))
+        {
+            var entityType = Path.GetFileName(entityTypeDirectory);
+            foreach (var entityFilename in Directory.GetFiles(entityTypeDirectory))
+            {
+                var entityName = Path.GetFileNameWithoutExtension(Path.GetFileName(entityFilename));
+                entries.Add((entityType, entityName));
+            }
+        }
+
+        return entries;
+    }
+
+    private static void SaveEntity(string rootDirectory, EntityTypeName entityTypeName, EntityName entityName, string content)
+    {
+        Directory.CreateDirectory($"{rootDirectory}/{entityTypeName}");
+        File.WriteAllText($"{rootDirectory}/{entityTypeName}/{entityName}.json", content);
+    }
+
 }
